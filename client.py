3bf57ad50320b3374eb029cf09d9512a83f9a69d
ESMERALDA RUIZ PUJADAS <esmeralda.ruiz@ub.edu>
2026-03-17 14:58:28 +0100

    params errors fixed

--- client.py
+++ client.py
@@ -6,24 +6,25 @@ import yaml
 
 import flcore.datasets as datasets
 from flcore.client_selector import get_model_client
-from params import get_parser, validate_model_specific_args,generate_config_dict
+from params import get_parser,generate_config_dict
+#, validate_model_specific_args
 # Start Flower client but after the server or error
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        config_path = sys.argv[2]
-    else:
-        config_path = "config.yaml"
+    #if len(sys.argv) == 3:
+    #    config_path = sys.argv[2]
+    #else:
+    #    config_path = "config.yaml"
 
-    with open(config_path, "r") as f:
-        config = yaml.safe_load(f)
+    #with open(config_path, "r") as f:
+    #    config = yaml.safe_load(f)
 
 
     #Instead of using the config.yaml use the parameters
-    #parser = get_parser(isserver=False)
-    #args = parser.parse_args()
-    #validate_model_specific_args(args)
-    #config = generate_config_dict(args,False)
+    parser = get_parser(isserver=False)
+    args = parser.parse_args()
+    ##validate_model_specific_args(args)
+    config = generate_config_dict(args,False)
 
 
 
